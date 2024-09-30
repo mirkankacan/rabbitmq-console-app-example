c4883c8a12fd8dd5bdce3515f7b7735d8e49b47f
Mirkan Kaçan <kacanmirkan@gmail.com>
2024-09-30 14:53:47 +0300

    Add project files.

---
+++ Receiver/RabbitMQExample.Receiver/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQExample.Receiver
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://ibfnxmgi:505o89xyJclBwAjh7--cEwAy17AU6OMk@shrimp.rmq.cloudamqp.com/ibfnxmgi");
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("message-queue", true, false, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume("message-queue", true, consumer);
+
+            consumer.Received += Consumer_Received;
+
+            Console.ReadLine();
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            Console.WriteLine("The message is: " + Encoding.UTF8.GetString(e.Body.ToArray()));
+        }
+    }
+}
---
+++ Sender/RabbitMQExample.Sender/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQExample.Sender
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://ibfnxmgi:505o89xyJclBwAjh7--cEwAy17AU6OMk@shrimp.rmq.cloudamqp.com/ibfnxmgi");
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("message-queue", true, false, false);
+
+            var message = "Hello World";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty, "message-queue", null, body);
+
+            Console.WriteLine("Message sent!");
+            Console.ReadLine();
+        }
+    }
+}
